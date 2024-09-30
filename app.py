3cb618e5fea1d3f477be0c14c4c2820c3f992057
CESARDELATORRE <cesardl@microsot.com>
2024-09-30 13:53:42 -0400

    Changes to GitHub Workflow deployment

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+import os
 from iebank_api import app
 
+# OLD/Original code (Uses port 5000 by default)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# Get the port from the PORT environment variable
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
